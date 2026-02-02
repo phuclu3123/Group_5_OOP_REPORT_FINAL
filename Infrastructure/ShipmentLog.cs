2162ba16c1f948aff64dfc6b78e7bda5382a3609
Truc Thanh <tructhanh6706@gmail.com>
2026-02-02 17:58:01 +0700

    Add Infrastructure folder and core classes

---
+++ Infrastructure/ShipmentLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group_OOP_FINAL.Infrastructure
+{
+    public class ShipmentLog
+    {
+        public string LogId { get; set; }
+        public string TrackingNumber { get; set; }
+        public string Location { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Status { get; set; }
+
+        public ShipmentLog(string logId, string trackingNumber, string location, string status)
+        {
+            LogId = logId;
+            TrackingNumber = trackingNumber;
+            Location = location;
+            Status = status;
+            Timestamp = DateTime.Now;
+        }
+    }
+}
---
+++ Infrastructure/Warehouse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group_OOP_FINAL.Infrastructure
+{
+    public class Warehouse
+    {
+        public string WarehouseId { get; set; }
+        public double Capacity { get; set; }
+        public double CurrentLoad { get; set; }
+
+        public List<WarehouseLocation> Locations { get; set; }
+
+        public Warehouse(string warehouseId, double capacity)
+        {
+            WarehouseId = warehouseId;
+            Capacity = capacity;
+            CurrentLoad = 0;
+            Locations = new List<WarehouseLocation>();
+        }
+    }
+}
