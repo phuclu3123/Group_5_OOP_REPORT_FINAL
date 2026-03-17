5ede3c41addb959a2ac95d337a72fa52436737ea
NGUYEN HOANG ANH <nguyenhoanganh88990@gmail.com>
2026-03-17 13:54:56 +0700

    Xoa het code cu de push lai file can thiet

--- Interfaces/IReportable.cs
+++
@@ -1,9 +0,0 @@
-namespace Cuoi_ky_OOP.Models.Interfaces
-{
-    // Interface bao cao - ap dung cho cac doi tuong co the tao bao cao tong hop
-    public interface IReportable
-    {
-        // Tao bao cao chi tiet
-        string GenerateReport();
-    }
-}
